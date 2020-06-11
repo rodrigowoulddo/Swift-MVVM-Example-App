16b4ba43492aafa82e1808096452a950ff705834
Rodrigo <rodrigofgiglio@gmail.com>
2020-06-11 13:24:00 -0300

    creates default xcode project

---
+++ Teste-Dev-iOS-Woop-SicrediTests/Teste_Dev_iOS_Woop_SicrediTests.swift
@@ -0,0 +1,34 @@
+//
+//  Teste_Dev_iOS_Woop_SicrediTests.swift
+//  Teste-Dev-iOS-Woop-SicrediTests
+//
+//  Created by Rodrigo Giglio on 11/06/20.
+//  Copyright © 2020 Rodrigo Giglio. All rights reserved.
+//
+
+import XCTest
+@testable import Teste_Dev_iOS_Woop_Sicredi
+
+class Teste_Dev_iOS_Woop_SicrediTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
